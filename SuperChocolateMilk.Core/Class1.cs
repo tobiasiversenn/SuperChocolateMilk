a21a29d58d4039ba9256c41e06614b88cc8938a2
tobiasiversenn <tobive01@easv.dk>
2026-09-14 13:47:06 +0200

    Scaffolded solution structure and project references (closes #1)

---
+++ SuperChocolateMilk.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace SuperChocolateMilk.Core;
+
+public class Class1
+{
+
+}
---
+++ SuperChocolateMilk.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperChocolateMilk.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
